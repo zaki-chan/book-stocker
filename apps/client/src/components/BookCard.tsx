b76a2ce16168dd3c96c51179a5664df0136aeefc
Zaki <126391961+zaki-chan@users.noreply.github.com>
2025-11-28 11:10:29 +0900

    Clean up BookCard component
    Removed commented-out code and unused imports in BookCard component.

--- apps/client/src/components/BookCard.tsx
+++ apps/client/src/components/BookCard.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import React from 'react'
 import { BookInfoResult } from '../../../../packages/common-types/src/BooksAPItypes'
 import RecordActionButton from './RecordButton'
-// import styles from './BookCard.module.css'
-import { Button } from '../../components/ui/button'
 import { Card, CardHeader, CardContent, CardFooter, CardTitle, CardDescription} from '../../components/ui/card'
 // 書籍情報を受け取るためのインターフェース
 interface BookCardProps {
@@ -24,27 +22,7 @@ export default function BookCard({bookInfo}:BookCardProps){
 
     // 書籍画像と情報を表示する
     return(
-        <div className=''>
-            {/* <Image
-                src={bookInfo.coverUrl as string}
-                width={200}
-                height={280}
-                alt={bookInfo.title}
-                className=''
-            />
-            <div className=''>
-                <ul className=''>
-                    <li>書籍名：{bookInfo.title}</li>
-                    <li>著者：{bookInfo.author}</li>
-                    <li>出版社：{bookInfo.publisher}</li>
-                    <li>{bookInfo.isRegistered ? '登録済み': '未登録'}</li>
-                </ul>
-                <Button
-                    variant="outline"
-                    onClick={handleClick}>
-                        登録
-                </Button>
-            </div> */}
+        <div>
             <Card className={`flex flex-col h-full ${registrationStatus}`}>
                 <CardHeader className='flex flex-row items-start space-x-4 pb-2'>
                     {bookInfo.coverUrl && (
